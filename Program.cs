47ad35a0f7db2a432204e58f7eabc320111289e3
Theo Lee <theo.leehao@elev.ga.ntig.se>
2023-11-20 09:27:30 +0100

    namn loop

--- Program.cs
+++ Program.cs
@@ -1,5 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Welcome, challenger");
+Thread.Sleep(1000);
+Console.WriteLine("To the maze of death");
+Thread.Sleep(1000);
+System.Console.WriteLine("Enter the name to write on your tomb");
+
+
+while (true)
+{
+    string name = "";
+    if (name != "")
+    {
+        break;
+    }
+    else if (name == "")
+    {
+        System.Console.WriteLine("Invaild input");
+    }
+}
+
+
+
+
+
+string player = Console.ReadLine();
+
+// Random generator = new Random();
+// int rounds = generator.Next(3,6);
+
+// List<string> enemies = new List<string>();
+
+Console.WriteLine("You enter the maze and approach a fork in the road. Which way do you choose? Left or Right");
+string a = Console.ReadLine().ToLower();
+
 
 
 Console.ReadLine();
